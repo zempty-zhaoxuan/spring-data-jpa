576bbb766f9ac2ff75fb02dee1a4a960a73f4445
zempty <kickcodeman@gmail.com>
2020-02-17 15:59:56 +0800

    add spring data jpa learning demo

---
+++ src/main/java/com/zempty/springbootjpa/SpringbootjpaApplication.java
@@ -0,0 +1,13 @@
+package com.zempty.springbootjpa;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringbootjpaApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringbootjpaApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/zempty/springbootjpa/SpringbootjpaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.zempty.springbootjpa;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringbootjpaApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
