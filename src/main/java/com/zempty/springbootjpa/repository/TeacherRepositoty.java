fdc6c218ea0b41c58dd81b27506adc17cca631d3
zempty <kickcodeman@gmail.com>
2020-02-29 16:08:14 +0800

    finish page function

--- src/main/java/com/zempty/springbootjpa/repository/TeacherRepositoty.java
+++ src/main/java/com/zempty/springbootjpa/repository/TeacherRepositoty.java
@@ -1,16 +1,23 @@
 package com.zempty.springbootjpa.repository;
 
-import com.zempty.springbootjpa.entity.Student;
 import com.zempty.springbootjpa.entity.Teacher;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.domain.Sort;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 
 import java.util.List;
 
 public interface TeacherRepositoty extends JpaRepository<Teacher,Integer> {
 
-    // 正常使用，只是多加了一个 sort 参数而已
+    // 正常使用，只是多加了一个 Sort 参数而已
     @Query("select t from Teacher t where t.subject = ?1")
     List<Teacher> getTeachers(String subject, Sort sort);
+
+
+    //正常使用，只是多加了一个 Pageable 参数而已
+    @Query("select t from Teacher t where t.subject = :subject")
+    Page<Teacher> getPage(@Param("subject") String subject, Pageable pageable);
 }
